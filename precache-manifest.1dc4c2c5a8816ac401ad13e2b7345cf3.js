9acb4476cc6240d13e778fec9650cca3fb7b4ee4
tawiii <4etkiu94@mail.ru>
2019-01-25 12:05:12 +0200

    Updates

---
+++ precache-manifest.1dc4c2c5a8816ac401ad13e2b7345cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d94bee7f8d109fa7ae6",
+    "url": "/jellyfish/static/js/runtime~main.0d94bee7.js"
+  },
+  {
+    "revision": "5fc87a81f1ebb11729c2",
+    "url": "/jellyfish/static/js/main.5fc87a81.chunk.js"
+  },
+  {
+    "revision": "fadc47b50e449bf01d5b",
+    "url": "/jellyfish/static/js/1.fadc47b5.chunk.js"
+  },
+  {
+    "revision": "5fc87a81f1ebb11729c2",
+    "url": "/jellyfish/static/css/main.a4603478.chunk.css"
+  },
+  {
+    "revision": "93c5e119bd11e989faebc82e778ce619",
+    "url": "/jellyfish/index.html"
+  }
+];
